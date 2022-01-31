3bfd71f677cbc7207c3af83fba526523bf75396a
Lucas Nolêto <lucasnoleto@outlook.pt>
2022-01-31 17:43:22 -0300

    Versão 1.0 - Erro em , não carrega os dados em sí!

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+use \src\handlers\UserHandler;
+
+class HomeController extends Controller {
+
+	private $loggedUser;
+
+	public function __construct(){
+
+		$this->loggedLogin = UserHandler::checkLogin();
+		if($this->loggedUser === false){
+
+			$this->redirect('/login');
+
+		}
+
+	}
+
+    public function index() {
+        $this->render('home', [
+
+        	'loggedUser' => $this->loggedUser
+
+        ]);
+    }
+
+}
---
+++ src/routes.php
@@ -0,0 +1,17 @@
+<?php
+use core\Router;
+
+$router = new Router();
+
+$router->get('/', 'HomeController@index');
+$router->get('/login','LoginController@signin');
+$router->post('/login','LoginController@signinAction');
+$router->get('/cadastro','LoginController@signup');
+$router->post('/cadastro','LoginController@signupAction');
+
+//$router->get('/pesquisa');
+//$router->get('/perfil');
+//$router->get('/sair');
+//$router->get('/amigos');
+//$router->get('/fotos');
+//$router->get('/config');
